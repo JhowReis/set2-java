9647746c74131ca2c642ebc268bdd9024e68d0dd
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2021-06-22 11:36:23 -0300

    Ending implementations

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,53 @@
 package application;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+import entities.CodeEntry;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+
+		Set<CodeEntry> set = new HashSet<>();
+
+		System.out.print("How many students for course A:  ");
+		int a = sc.nextInt();
+		for (int i = 1; i <= a; i++) {
+			System.out.print("code " + i + ": ");
+			int code = sc.nextInt();
+			set.add(new CodeEntry(code));
+			if (i == a) {
+				System.out.print("How many students for course B:  ");
+				int b = sc.nextInt();
+				for (i = 1; i <= b; i++) {
+					System.out.print("code " + i + ": ");
+					code = sc.nextInt();
+					set.add(new CodeEntry(code));
+					if (i == b) {
+						System.out.print("How many students for course C:  ");
+						int c = sc.nextInt();
+						for (i = 1; i <= c; i++) {
+							System.out.print("code " + i + ": ");
+							code = sc.nextInt();
+							set.add(new CodeEntry(code));
+							if (i == c) {
+								System.out.println("Total students" + set.size());
+							}
+						}
+
+					}
+
+				}
+
+			}
+
+		}
+		
+
+		sc.close();
 	}
-
 }
--- src/entities/CodeEntry.java
+++ src/entities/CodeEntry.java
@@ -1,5 +1,47 @@
 package entities;
 
 public class CodeEntry {
+	
+	private Integer code;
+
+	public CodeEntry(Integer code) {
+		super();
+		this.code = code;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((code == null) ? 0 : code.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CodeEntry other = (CodeEntry) obj;
+		if (code == null) {
+			if (other.code != null)
+				return false;
+		} else if (!code.equals(other.code))
+			return false;
+		return true;
+	}
+	
+	
 
 }
